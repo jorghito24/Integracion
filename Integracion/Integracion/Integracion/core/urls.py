a5e4d73e673462387111b2485b1234dcfac2a037
Feli252002 <130710309+Feli252002@users.noreply.github.com>
2023-04-27 18:34:09 -0400

    Add files via upload

--- Integracion/Integracion/Integracion/core/urls.py
+++ Integracion/Integracion/Integracion/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-from core.views import index, vistadmin, vistaresidente, login,nosotros
+from core.views import index, vistadmin, login,nosotros
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
@@ -8,7 +8,6 @@ urlpatterns = [
     
     path('',views.index,name='index'),
     path('vistadmin',views.vistadmin,name='vistadmin'),
-    path('vistaresidente',views.vistaresidente,name='vistaresidente'),
     path('login',views.login,name='login'),
     path('nosotros',views.nosotros,name='nosotros'),
     
