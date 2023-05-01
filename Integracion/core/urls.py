e823f689c32b5781aaa8051a3f0379dc338cadd9
jorge <jorgefreire2403@gmail.com>
2023-05-01 16:45:34 -0400

    cambio 01/05

--- Integracion/core/urls.py
+++ Integracion/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-from core.views import index, vistadmin, login,nosotros,vistaresidente
+from core.views import index, vistadmin,nosotros,vistaresidente, login
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
@@ -8,7 +8,7 @@ urlpatterns = [
     
     path('',views.index,name='index'),
     path('vistadmin/',views.vistadmin,name='vistadmin'),
-    path('login',views.login,name='login'),
+    path('login/',views.login,name='login'),
     path('nosotros/',views.nosotros,name='nosotros'),
     path('vistaresidente/', views.vistaresidente, name='vistaresidente'),
     
